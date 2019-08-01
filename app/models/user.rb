0e5a1df8dfe1064aa670015db09958e912c82223
github username <github email address>
2019-08-01 12:41:19 -0400

    fixed relationships, and reiew feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates_uniqueness_of :email
+
+    
     belongs_to :workspace
-    has_many :user_ratings, foreign_key: :review_user_id
-    has_many :rated_users, through: :user_ratings
-    has_many :rated_user_reviews, foreign_key: :coworker_id, class_name: "Review"
+    has_many :reviews 
+
 end
--- db/migrate/20190729205226_create_reviews.rb
+++ db/migrate/20190729205226_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.belongs_to :user, foreign_key: true
       t.integer :rating
       t.string :comment
+      t.string :reviewer_name
 
       t.timestamps
     end
